0ac57cfa296885e9fa2e474c25c70f6538146f40
erezrokah <erezrokah@users.noreply.github.com>
2021-02-11 19:24:44 +0100

    Release Version 0.1.10

--- git-credential-netlify.rb
+++ git-credential-netlify.rb
@@ -1,9 +1,9 @@
 class GitCredentialNetlify < Formula
   desc "Git Credential Helper using Netlify's API for authentication"
   homepage "https://github.com/netlify/netlify-credential-helper"
-  url "https://github.com/netlify/netlify-credential-helper/releases/download/v0.1.9/git-credential-netlify-darwin-amd64.tar.gz"
-  version "0.1.9"
-  sha256 "5feccd51f3fc097ca482dc181fe68c71a0c91e1fbf37c2f92126f0fd0b6d3a14"
+  url "https://github.com/netlify/netlify-credential-helper/releases/download/v0.1.10/git-credential-netlify-darwin-amd64.tar.gz"
+  version "0.1.10"
+  sha256 "6025d831aa4433c58bf87c7d629bcb332035d144e90bbd9bba863fe8fe68b0aa"
 
   def install
     bin.install "git-credential-netlify"
